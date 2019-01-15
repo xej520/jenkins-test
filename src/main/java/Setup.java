684d0905e8244f37b51f76fec392d5e13f5bf0a7
邢尔军 <xingej@163.com>
2019-01-15 11:19:35 +0800

    更新启动命令

--- src/main/java/Setup.java
+++ src/main/java/Setup.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -5,11 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Setup {
+	private static Logger logger = LoggerFactory.getLogger(Setup.class);
 
 	public static void main(String[] args) {
 
-		System.out.println("----jenkinks");
-
+		logger.info("----代码托管:\tgithub");
+		logger.info("----语言:\tjava");
+		logger.info("----编译工具:\tmaven");
+		SpringApplication.run(Setup.class, args);
 	}
 
 }
